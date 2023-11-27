c8337783fe1f5e85d13b6e378b61ad70413632ae
longfengpili <398745129@qq.com>
2023-11-27 15:36:10 +0800

    [新增]docker volumes

--- bat/mkvolumes.bat
+++ bat/mkvolumes.bat
@@ -1,7 +1,7 @@
 @REM @Author: longfengpili
 @REM @Date:   2023-11-20 18:40:29
 @REM @Last Modified by:   longfengpili
-@REM Modified time: 2023-11-23 11:14:26
+@REM Modified time: 2023-11-27 15:13:55
 
 @echo off
 
@@ -13,3 +13,7 @@ mkdir .\volumes\postgresql\data
 mkdir .\volumes\hadoop\dfs\name
 mkdir .\volumes\hadoop\dfs\data1
 mkdir .\volumes\hadoop\dfs\data2
+
+mkdir .\volumes\homes\mhome
+mkdir .\volumes\homes\w1home
+mkdir .\volumes\homes\w2home
