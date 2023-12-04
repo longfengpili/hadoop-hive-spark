6fdd74146a305db2da619a37aafc2518a58e9d61
longfengpili <398745129@qq.com>
2023-12-04 14:23:16 +0800

    [新增]outputdata

--- bat/mkvolumes.bat
+++ bat/mkvolumes.bat
@@ -1,7 +1,7 @@
 @REM @Author: longfengpili
 @REM @Date:   2023-11-20 18:40:29
 @REM @Last Modified by:   longfengpili
-@REM Modified time: 2023-11-27 16:14:27
+@REM Modified time: 2023-12-04 14:03:28
 
 @echo off
 
@@ -14,6 +14,4 @@ mkdir .\volumes\hadoop\dfs\name
 mkdir .\volumes\hadoop\dfs\data1
 mkdir .\volumes\hadoop\dfs\data2
 
-rem mkdir .\volumes\homes\mhome
-rem mkdir .\volumes\homes\w1home
-rem mkdir .\volumes\homes\w2home
+mkdir .\volumes\workers\outputdata
