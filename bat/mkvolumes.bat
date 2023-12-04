803adce3e993bff8b1388feeb2ee1eccaf9e1a69
longfengpili <398745129@qq.com>
2023-12-04 15:14:44 +0800

    [新增]jupyter output

--- bat/mkvolumes.bat
+++ bat/mkvolumes.bat
@@ -1,7 +1,7 @@
 @REM @Author: longfengpili
 @REM @Date:   2023-11-20 18:40:29
 @REM @Last Modified by:   longfengpili
-@REM Modified time: 2023-12-04 14:03:28
+@REM Modified time: 2023-12-04 15:13:52
 
 @echo off
 
@@ -14,4 +14,4 @@ mkdir .\volumes\hadoop\dfs\name
 mkdir .\volumes\hadoop\dfs\data1
 mkdir .\volumes\hadoop\dfs\data2
 
-mkdir .\volumes\workers\outputdata
+mkdir .\volumes\outputdata
